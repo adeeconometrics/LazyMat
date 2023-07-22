be2cb2072fb13a3eb93657ef463ab534db5e0d90
ddamiana <amianism25@gmail.com>
2023-07-22 19:21:56 +0800

    initial commit to utils module

---
+++ include/Utils.hpp
@@ -0,0 +1,49 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <type_traits>
+#include <vector>
+
+namespace lm {
+
+class Timer {
+public:
+  Timer() : start_time(std::chrono::high_resolution_clock::now()) {}
+  ~Timer() {
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+                        end_time - start_time)
+                        .count();
+    std::cout << "Elapsed time: " << duration << " microseconds\n";
+  }
+
+private:
+  std::chrono::high_resolution_clock::time_point start_time;
+};
+
+template <typename T, typename std::enable_if_t<std::is_arithmetic_v<T>>>
+auto make_matrix(const std::size_t row, const std::size_t col,
+                 std::reference_wrapper<std::mt19937> prng)
+    -> std::vector<std::vector<T>> {
+
+  std::vector<std::vector<T>> result;
+  result.reserve(row);
+
+  for (std::size_t i = 0; i < row; i++) {
+    std::vector<T> row;
+    row.reserve(col);
+    std::generate_n(std::back_inserter(row), col, prng);
+    result.emplace_back(row);
+  }
+
+  return result;
+}
+
+} // namespace lm
+
+#endif // __UTILS_H__
