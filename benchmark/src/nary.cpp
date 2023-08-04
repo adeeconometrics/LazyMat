a5863592708bf0cc153ea5131d40496bc824258e
ddamiana <amianism25@gmail.com>
2023-08-04 19:32:11 +0800

    savepoint for functor idea on nary

--- benchmark/src/nary.cpp
+++ benchmark/src/nary.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <functional>
 #include <initializer_list>
+#include <iostream>
 #include <random>
 #include <stdexcept>
 #include <type_traits>
@@ -20,62 +21,81 @@ auto subscript(const operand &v, const std::size_t index) {
   return v;
 }
 
+// template <typename Fn, typename RType, typename ... Args>
+// struct Callable {
+
+//   auto operator()(const Args &... args) -> RType {
+
+//   }
+// };
+
+template <typename RType, typename... Args> class expr {
+  std::tuple<Args const &...> m_args;
+  std::function<RType(Args...)> m_fn;
+
+public:
+  expr(std::function<RType(Args...)> t_fn, const Args &... t_args)
+      : m_args(t_args...), m_fn(t_fn) {}
+
+  auto operator()(std::size_t i) const noexcept -> RType {
+    const auto call_at_index = [this, i](const Args &... a) {
+      return m_fn(subscript(a, i)...);
+    };
+    return std::apply(call_at_index, m_args);
+  }
+  // template <typename T> auto operator[](std::size_t i) const noexcept ->
+  // RType {
+  //   const auto call_at_index = [this, i](const Args &... a) {
+  //     return m_fn(subscript(a, i)...);
+  //   };
+  //   return expr{call_at_index, std::get<const Args &>(m_args)...};
+  // }
+
+  // template <typename T> operator T() const {
+  //   return m_fn(std::get<const Args &>(m_args)...);
+  // }
+};
+
+template <typename Lhs, typename Rhs>
+constexpr auto operator*(Lhs const &lhs, Rhs const &rhs) {
+  return expr{
+      [](const auto &l, const auto &r) -> decltype(l * r) { return l * r; },
+      lhs, rhs};
+}
+
 template <typename T, std::size_t N> struct lazy_vector {
 
+  lazy_vector() = default;
   lazy_vector(const std::array<T, N> &t_data) : m_data(t_data) {}
 
-  template <typename Expr> auto operator=(Expr rhs) -> lazy_vector<T, N> & {
+  template <typename RType, typename... Args>
+  auto operator=(expr<RType, Args...> rhs) -> lazy_vector<T, N> & {
     for (std::size_t i = 0; i < N; i++) {
       m_data[i] = rhs[i];
     }
     return *this;
   }
 
-private:
-  std::array<T, N> m_data;
-};
-
-template <typename callable, typename... operands> class expr {
-  std::tuple<operands const &...> m_args;
-  callable m_fn;
-
-public:
-  expr(callable t_fn, const operands &... t_args)
-      : m_args(t_args...), m_fn(t_fn) {}
-
-  auto operator[](const std::size_t i) const noexcept {
-    const auto call_at_index = [this, i](const operands &... a) {
-      return m_fn(subscript(a, i)...);
-    };
-    return std::apply(call_at_index, m_args);
+  auto operator[](std::size_t index) const noexcept -> T {
+    return m_data[index];
   }
+
+  auto operator[](std::size_t index) -> T { return m_data[index]; }
+
+private:
+  std::array<T, N> m_data{};
 };
-
-template <typename Lhs, typename Rhs>
-auto operator*(Lhs const &lhs, Rhs const &rhs) {
-  return expr{[](const auto &l, const auto &r) { return l * r; }, lhs, rhs};
-}
-
-template <typename Lhs, typename Rhs>
-auto operator+(Lhs const &lhs, Rhs const &rhs) {
-  return expr{[](const auto &l, const auto &r) { return l + r; }, lhs, rhs};
-}
-
-template <typename Lhs, typename Rhs>
-auto operator-(Lhs const &lhs, Rhs const &rhs) {
-  return expr{[](const auto &l, const auto &r) { return l - r; }, lhs, rhs};
-}
 
 auto main() -> int {
   std::mt19937 rng_a(64);
   std::mt19937 rng_b(65);
 
-  auto A = lazy_vector{make_vector<int, 256>(std::ref(rng_a))};
-  auto B = lazy_vector{make_vector<int, 256>(std::ref(rng_b))};
+  auto A = lazy_vector<int, 64>{make_vector<int, 64>(std::ref(rng_a))};
+  auto B = lazy_vector<int, 64>{make_vector<int, 64>(std::ref(rng_b))};
 
-  lazy_vector<int, 256> C;
+  lazy_vector<int, 64> C{};
   {
     Timer t;
-    C = A * B * B + A * B + A * A * B;
+    C = A * B;
   }
 }
