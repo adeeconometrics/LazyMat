9cdd1c9bbcaa0d8900a43fa6946d778dcc52b4e6
ddamiana <amianism25@gmail.com>
2024-02-07 22:29:31 +0800

    modified time utils and implement omp to paged vectors

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,23 @@ auto main() -> int {
   std::mt19937 rng_a(67);
   std::mt19937 rng_b(65);
 
-  Matrix<int, 128, 128> A{make_vmatrix<int, 128, 128>(std::ref(rng_a))};
-  Matrix<int, 128, 128> B{make_vmatrix<int, 128, 128>(std::ref(rng_b))};
+  constexpr int M = 1024;
+  constexpr int N = 1024;
+  constexpr int K = 1024;
 
-  Matrix<int, 128, 128> C;
+  Matrix<int, M, N> A{make_vmatrix<int, M, N>(std::ref(rng_a))};
+  Matrix<int, M, K> B{make_vmatrix<int, M, K>(std::ref(rng_b))};
+
+  Matrix<int, K, N> C;
   {
-    Timer t;
-    C = A * B * B + A * B + A * A * B;
+    const std::size_t iterations = 3;
+    Timer t{iterations};
+    for (std::size_t i = 0; i < iterations; i++) {
+      t.start();
+      C = A * B + B / A * B * B - A;
+    }
   }
-  // std::cout << C;
+
   // B = log(A);
 
   return 0;
