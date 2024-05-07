c00ac4c0dbfc0fe1afbe5a0d3f3fdd337161c1c7
Dave Amiana <amianism25@gmail.com>
2024-05-07 23:26:28 +0800

    added new expr for comparison

--- benchmark/linalg_libs/gflops.py
+++ benchmark/linalg_libs/gflops.py
@@ -4,38 +4,46 @@ import numpy as np
 import torch as pt
 import tensorflow as tf
 
-def get_gflops(ops:float, seconds:float) -> float:
+
+def get_gflops(ops: float, seconds: float) -> float:
     return (ops/seconds)/1e9
 
-def make_table(t_values:dict) -> None:
+
+def make_table(t_values: dict) -> None:
     print(tabulate(t_values, headers=("lib", "gflop/s", "secs", "relative throughput")))
+
 
 if __name__ == "__main__":
     table = []
-    equation = lambda A,B:  A * B + B / A * B * B - A
-    M,N,K = 2048,2048,2048
-    nsec = 41*1e-9
-    
-    lm_gflops = get_gflops(N*M*6, nsec)
+    def equation(A, B): return A * B + B / A * B * B - A
+    M, N, K = 256, 256, 256
+    nsec = 8112472*1e-9
+
+    floating_ops = 4*M*N*K + 2*M*N
+    lm_gflops = get_gflops(floating_ops, nsec)
     table.append(("lazy_mat", lm_gflops, nsec, '---'))
 
-    np_A = np.random.rand(M,N)
-    np_B = np.random.rand(M,K)
-    np_sec = timeit(lambda: equation(np_A,np_B), number=2)/2
-    np_gflops = get_gflops(N*M*6, np_sec)
-    table.append(("numpy",get_gflops(N*M*6, np_gflops),np_sec, f'{lm_gflops/np_gflops : .4f}x'))
+    np_A = np.random.rand(M, N)
+    np_B = np.random.rand(M, K)
+    np_sec = timeit(lambda: np_A@np_B + np_A * np_B * np_A@np_B, number=3)/3
+    np_gflops = get_gflops(floating_ops, np_sec)
+    table.append(("numpy", get_gflops(floating_ops, np_gflops),
+                 np_sec, f'{lm_gflops/np_gflops: .4f}x'))
 
     pt_A = pt.tensor(np_A)
     pt_B = pt.tensor(np_B)
-    pt_sec = timeit(lambda: equation(pt_A,pt_B), number=2)/2
-    pt_gflops = get_gflops(N*M*6, pt_sec)
-    table.append(("pytorch",pt_gflops, pt_sec, f'{lm_gflops/pt_gflops : .4f}x'))
-
+    pt_sec = timeit(lambda: pt.matmul(pt_A, pt_B) + pt_A *
+                    pt_B * pt.matmul(pt_A, pt_B), number=3)/3
+    pt_gflops = get_gflops(floating_ops, pt_sec)
+    table.append(("pytorch", pt_gflops, pt_sec,
+                 f'{lm_gflops/pt_gflops: .4f}x'))
 
     tf_A = tf.convert_to_tensor(np_A)
     tf_B = tf.convert_to_tensor(np_B)
-    tf_sec = timeit(lambda: equation(tf_A,tf_B), number=2)/2
-    tf_gflops = get_gflops(N*M*6, tf_sec)
-    table.append(("tensorflow",tf_gflops, tf_sec, f'{lm_gflops/tf_gflops : .4f}x'))
+    tf_sec = timeit(lambda: tf.matmul(tf_A, tf_B) + tf_A *
+                    tf_B * tf.matmul(tf_A, tf_B), number=3)/3
+    tf_gflops = get_gflops(floating_ops, tf_sec)
+    table.append(("tensorflow", tf_gflops, tf_sec,
+                 f'{lm_gflops/tf_gflops: .4f}x'))
 
     make_table(table)
